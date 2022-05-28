e088dcac0fec039a5c01725a849d33d49441cefb
Sandra Calero <3358@holbertonschool.com>
2022-05-27 19:15:22 -0500

    UPD trash bin icons

--- src/components/molecules/TrashBinLink/TrashBinLink.tsx
+++ src/components/molecules/TrashBinLink/TrashBinLink.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
-import trashBinEmpty from 'assets/icons/trashBinEmpty.svg';
-/* import trashBinFull from 'assets/icons/trashBinFull.svg'; */
+import trashBinEmpty from 'assets/icons/trashBinEmpty.png';
+/* import trashBinFull from 'assets/icons/trashBinFull.png'; */
 import './TrashBinLink.css';
 
 export default function TrashBinLink() {
 	return (
 		<Link
 			to='/TrashSpace'
-			id='drop_zone'
+			className='trashBinLink'
 			onDrop={(event) => console.log(event)}
 			onDragOver={(event) => console.log(event)}
 		>
 			<img src={trashBinEmpty} alt='Trash bin' />
+			{/* <img src={trashBinFull} alt='Trash bin' /> */}
 		</Link>
 	);
 }
